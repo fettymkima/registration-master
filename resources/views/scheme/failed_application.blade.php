7b6d276c558372c0f3f86a78e6a68fb36debda0d
fatuma.mkima <fettymkima@gmail.com>
2018-12-06 15:32:12 +0300

    fees form

---
+++ resources/views/scheme/failed_application.blade.php
@@ -0,0 +1,121 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="flash-message">
+        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+            @if(Session::has('alert-' . $msg))
+
+                <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+            @endif
+        @endforeach
+    </div> <!-- end .flash-message -->
+
+    {{--get the tab selected from the controller in order to show it.--}}
+    {{--<input type="hidden"  value="{{$tab}}" id="tab-selected">--}}
+    {{--end get tab value--}}
+
+    <ul class="nav nav-tabs" id="myTab">
+        <li class="active">
+            <a  href="#1" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>General Details</a>
+        </li>
+        <li><a href="#2" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>Employer(s) Particulars
+            </a>
+        </li>
+        <li><a href="#3" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>Board of Trustee</a>
+        </li>
+        <li><a href="#4" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>Auditor and Legal Advisor</a>
+        </li>
+        <li><a href="#5" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>Reasons</a>
+        </li>
+        <li><a href="#6" data-toggle="tab"><span class="glyphicon glyphicon-th-large"></span>Attachements & Aggrements
+            </a>
+        </li>
+    </ul>
+
+    <div class="tab-content" style="  border-left: 1px solid #ddd;
+        border-right: 1px solid #ddd; border-bottom: 1px solid #ddd;
+        padding: 10px;" >
+
+        <div class="tab-pane active" id="1" style="background-color: white">
+            {{--@include('complaints.edit',compact('edit_complaints'))--}}
+
+        </div>
+
+        <div class="tab-pane" id="2" style="background-color: white">
+
+            {{--@include('complaints.open',compact('open_complaints'))--}}
+
+        </div>
+
+        <div class="tab-pane" id="3" style="background-color: white">
+
+            {{--@include('complaints.pending',compact('pending_complaints'))--}}
+
+        </div>
+
+        <div class="tab-pane" id="4" style="background-color: white;">
+
+            {{--@include('complaints.closed',compact('closed_complaints'))--}}
+
+        </div>
+
+        <div class="tab-pane" id="5" style="background-color: white;">
+
+            {{--@include('complaints.closed',compact('closed_complaints'))--}}
+
+        </div>
+        <div class="tab-pane" id="6" style="background-color: white;">
+
+            {{--@include('complaints.closed',compact('closed_complaints'))--}}
+
+        </div>
+
+
+    </div>
+
+@stop
+
+@section('css')
+
+    <style>
+        table.table-bordered{
+            border:1px ;
+            margin-top:20px;
+        }
+        table.table-bordered > thead > tr > th{
+            border:1px ;
+        }
+        table.table-bordered > tbody > tr > td{
+            border:1px ;
+        }
+    </style>
+@stop
+
+@section('js')
+
+    <script src="/complaints/js/tab_open.js"></script>
+    <script src="/complaints/js/tab_edit.js"></script>
+    <script src="/complaints/js/tab_pending.js"></script>
+    <script src="/complaints/js/tab_closed.js"></script>
+
+    <script>
+
+        $(function (e) {
+
+            var tab  = $("#tab-selected").val();
+
+            $('#myTab li:nth-child('+tab+') a').tab('show');
+
+
+
+        });
+    </script>
+
+
+@stop
+
+
+
+
